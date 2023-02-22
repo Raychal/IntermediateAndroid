83e891a0d69d86d4800af084aadb222202a2502e
raychal <abelrawz@gmail.com>
2023-02-22 11:45:03 +0700

    learn how to create custom button and edit

--- app/src/main/java/com/example/intermediateandroid/MainActivity.kt
+++ app/src/main/java/com/example/intermediateandroid/MainActivity.kt
@@ -2,15 +2,39 @@ package com.example.intermediateandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.Toast
 import com.example.intermediateandroid.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    private lateinit var myButton: MyButton
+    private lateinit var myEditText: MyEditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        setMyButtonEnable()
+
+        binding.myEditText.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
+            }
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+                setMyButtonEnable()
+            }
+            override fun afterTextChanged(s: Editable) {
+            }
+        })
+        binding.myButton.setOnClickListener { Toast.makeText(this@MainActivity, binding.myEditText.text, Toast.LENGTH_SHORT).show() }
+    }
+
+    private fun setMyButtonEnable() {
+        val result = binding.myEditText.text
+        binding.myButton.isEnabled = result != null && result.toString().isNotEmpty()
     }
 }
