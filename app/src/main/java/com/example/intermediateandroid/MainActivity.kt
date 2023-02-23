59b750ebc7d5bd47e303995b6297bf70c73f6f4e
raychal <abelrawz@gmail.com>
2023-02-23 15:30:21 +0700

    update master

--- app/src/main/java/com/example/intermediateandroid/MainActivity.kt
+++ app/src/main/java/com/example/intermediateandroid/MainActivity.kt
@@ -1,49 +1,16 @@
 package com.example.intermediateandroid
 
-import android.media.SoundPool
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
 import com.example.intermediateandroid.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private lateinit var sp: SoundPool
-    private var soundId: Int = 0
-    private var spLoaded = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        val btnSound = binding.btnSoundPool
-
-        sp = SoundPool.Builder()
-            .setMaxStreams(10)
-            .build()
-
-        /*
-        Tambahkan listener ke soundpool jika proses load sudah selesai
-         */
-        sp.setOnLoadCompleteListener { _, _, status ->
-            if (status == 0) {
-                spLoaded = true
-            } else {
-                Toast.makeText(this@MainActivity, "Gagal load", Toast.LENGTH_SHORT).show()
-            }
-        }
-
-        /*
-        Load raw clinking_glasses ke soundpool, jika selesai maka id nya dimasukkan ke variable soundId
-         */
-        soundId = sp.load(this, R.raw.clinking_glasses, 1) // in 2nd param u have to pass your desire ringtone
-
-        btnSound.setOnClickListener {
-            if (spLoaded) {
-                sp.play(soundId, 1f, 1f, 0, 0, 1f)
-            }
-        }
     }
 }
