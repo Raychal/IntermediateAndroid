9dab263bb41fca36abb29dd8d09f0ba27e8466e6
raychal <abelrawz@gmail.com>
2023-02-23 16:24:51 +0700

    learn how to create media player

--- app/src/main/java/com/example/intermediateandroid/MainActivity.kt
+++ app/src/main/java/com/example/intermediateandroid/MainActivity.kt
@@ -1,49 +1,66 @@
 package com.example.intermediateandroid
 
+import android.media.AudioAttributes
+import android.media.MediaPlayer
 import android.media.SoundPool
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import com.example.intermediateandroid.databinding.ActivityMainBinding
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private lateinit var sp: SoundPool
-    private var soundId: Int = 0
-    private var spLoaded = false
+    private var mMediaPlayer: MediaPlayer? = null
+    private var isReady: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val btnSound = binding.btnSoundPool
-
-        sp = SoundPool.Builder()
-            .setMaxStreams(10)
-            .build()
-
-        /*
-        Tambahkan listener ke soundpool jika proses load sudah selesai
-         */
-        sp.setOnLoadCompleteListener { _, _, status ->
-            if (status == 0) {
-                spLoaded = true
+        val btnPlay = binding.btnPlay
+        val btnStop = binding.btnStop
+        btnPlay.setOnClickListener {
+            if (!isReady) {
+                mMediaPlayer?.prepareAsync()
             } else {
-                Toast.makeText(this@MainActivity, "Gagal load", Toast.LENGTH_SHORT).show()
+                if (mMediaPlayer?.isPlaying as Boolean) {
+                    mMediaPlayer?.pause()
+                } else {
+                    mMediaPlayer?.start()
+                }
+            }
+        }
+        btnStop.setOnClickListener {
+            if (mMediaPlayer?.isPlaying as Boolean || isReady) {
+                mMediaPlayer?.stop()
+                isReady = false
             }
         }
 
-        /*
-        Load raw clinking_glasses ke soundpool, jika selesai maka id nya dimasukkan ke variable soundId
-         */
-        soundId = sp.load(this, R.raw.clinking_glasses, 1) // in 2nd param u have to pass your desire ringtone
+        init()
+    }
 
-        btnSound.setOnClickListener {
-            if (spLoaded) {
-                sp.play(soundId, 1f, 1f, 0, 0, 1f)
-            }
+    private fun init() {
+        mMediaPlayer = MediaPlayer()
+        val attribute = AudioAttributes.Builder()
+            .setUsage(AudioAttributes.USAGE_MEDIA)
+            .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+            .build()
+        mMediaPlayer?.setAudioAttributes(attribute)
+        val afd = applicationContext.resources.openRawResourceFd(R.raw.guitar_background)
+        try {
+            mMediaPlayer?.setDataSource(afd.fileDescriptor, afd.startOffset, afd.length)
+        } catch (e: IOException) {
+            e.printStackTrace()
         }
+
+        mMediaPlayer?.setOnPreparedListener {
+            isReady = true
+            mMediaPlayer?.start()
+        }
+        mMediaPlayer?.setOnErrorListener { _, _, _ -> false }
     }
 }
