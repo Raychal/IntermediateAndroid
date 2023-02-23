ba3e8fef256db12bc77fbba8e5b4a5525758f2ff
raychal <abelrawz@gmail.com>
2023-02-23 10:15:18 +0700

    starter pack localization multi language

--- app/src/main/java/com/example/intermediateandroid/MainActivity.kt
+++ app/src/main/java/com/example/intermediateandroid/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.intermediateandroid
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowInsets
+import android.view.WindowManager
 import com.example.intermediateandroid.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,5 +15,20 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        setupView()
+    }
+
+    private fun setupView() {
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+        supportActionBar?.hide()
     }
 }
