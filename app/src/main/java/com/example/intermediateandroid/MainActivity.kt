f08b0efca4adeffb6989760dd8758ce1a22bff68
raychal <abelrawz@gmail.com>
2023-02-23 13:28:09 +0700

    learn how to make service

--- app/src/main/java/com/example/intermediateandroid/MainActivity.kt
+++ app/src/main/java/com/example/intermediateandroid/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.intermediateandroid
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.intermediateandroid.databinding.ActivityMainBinding
@@ -12,5 +13,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val serviceIntent = Intent(this, MyBackgroundService::class.java)
+        binding.btnStartBackgroundService.setOnClickListener {
+            startService(serviceIntent)
+        }
+        binding.btnStopBackgroundService.setOnClickListener {
+            stopService(serviceIntent)
+        }
+
     }
 }
