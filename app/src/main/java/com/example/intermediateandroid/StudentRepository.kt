59f96094823cb7521770cc3ccc2920faaeb0f693
raychal <abelrawz@gmail.com>
2023-03-08 14:50:00 +0700

    learn how to make paging2 from database

--- app/src/main/java/com/example/intermediateandroid/StudentRepository.kt
+++ app/src/main/java/com/example/intermediateandroid/StudentRepository.kt
@@ -1,15 +1,23 @@
 package com.example.intermediateandroid
 
 import androidx.lifecycle.LiveData
+import androidx.paging.LivePagedListBuilder
+import androidx.paging.PagedList
 import com.example.intermediateandroid.database.*
 import com.example.intermediateandroid.helper.InitialDataSource
 import com.example.intermediateandroid.helper.SortType
 import com.example.intermediateandroid.helper.SortUtils
 
 class StudentRepository(private val studentDao: StudentDao) {
-    fun getAllStudent(sortType: SortType): LiveData<List<Student>> {
+    fun getAllStudent(sortType: SortType): LiveData<PagedList<Student>> {
         val query = SortUtils.getSortedQuery(sortType)
-        return studentDao.getAllStudent(query)
+        val student = studentDao.getAllStudent(query)
+        val config = PagedList.Config.Builder()
+            .setEnablePlaceholders(true)
+            .setInitialLoadSizeHint(30)
+            .setPageSize(10)
+            .build()
+        return LivePagedListBuilder(student, config).build()
     }
     fun getAllStudentAndUniversity(): LiveData<List<StudentAndUniversity>> = studentDao.getAllStudentAndUniversity()
     fun getAllUniversityAndStudent(): LiveData<List<UniversityAndStudent>> = studentDao.getAllUniversityAndStudent()
